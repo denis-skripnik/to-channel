eafc25c5804191c2bd80dc3734582aba0fcbf353
denis-skripnik <scadens@yandex.ru>
2023-09-05 10:09:29 +0700

    Add replace &nbsp; and delete forwarding

--- to_channel.js
+++ to_channel.js
@@ -3,6 +3,7 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 const {CHANNEL_ID, TELEGRAM_BOT_TOKEN, minutes, min_length} = require('./config.json');
+let channels_logins = [];
 
 const { Bot } = require("grammy");
 const bot = new Bot(TELEGRAM_BOT_TOKEN);
@@ -36,13 +37,15 @@ async function scrapeAndSend(login, only_keywords, not_keywords) {
       if (post.text === 'Live stream started') {
         post.text = `<a href="https://t.me/${login}?livestream">${post.text}</a>`;
       }
+      if (channels_logins.some(function(v) { return post.text.indexOf(v) >= 0; })) continue;
       const htmlMessage = `<a href="${post.url}">${login}</a>\n${post.text}
 
 ${CHANNEL_ID}`;
 const allowedTags = ['a', 'b', 'strong', 'i', 'em', 'code', 'pre', 'strike', 'del', 'u', 's', 'br', 'p'];
 const message = htmlMessage
   .replace(/<(\/)?((?!\/?\s*)?(\b(?:a|b|strong|i|em|code|pre|strike|del|u|s|br|p)\b))(?:\s+(?!href)\S+="[^"]*")*(\s*href="[^"]*")?(?:(?:(?<=\s)\/)?>|(?<=\S)\/>)/gi, '<$1$2$4>')
-  .replace(/(<p>|<\/p>|<br\/>|<br>)/gi, '\n');
+  .replace(/(<p>|<\/p>|<br\/>|<br>)/gi, '\n')
+  .replace('&nbsp;', '');
   if (message.length < min_length || message.indexOf('null') > -1) continue;
                 const lowerCaseMessage = message.toLowerCase();
       if (only_keywords && only_keywords.length > 0 && only_keywords[0] !== '' && !only_keywords.some(keyword => lowerCaseMessage.includes(keyword))) continue;
@@ -59,6 +62,7 @@ const message = htmlMessage
 
 async function main() {
 const channels = JSON.parse(fs.readFileSync('channels.json', 'UTF-8'))
+channels_logins = Object.keys(channels);
 
     for (let login in channels) {
     let search = (channels[login].only_keywords).split(',');
